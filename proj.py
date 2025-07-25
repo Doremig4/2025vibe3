3af37dc506ce4dc6b44f30123dfdf189909194eb
Doremig4 <95349542+Doremig4@users.noreply.github.com>
2025-07-25 15:03:31 +0900

    ㅓㅓㅓ

--- proj.py
+++ proj.py
@@ -4,9 +4,8 @@ import plotly.express as px
 
 st.set_page_config(page_title="자살률(연령별) 통계 시각화", layout="wide")
 st.title("📊 자살률(연령별) 통계 시각화")
-st.write("출처: 통계청 | 단위: 명, 자살률")
+st.write("출처: 통계청 | 단위: 명")
 
-# 업로드 없이 로컬 파일 자동 불러오기
 csv_path = "자살률(연령별)_20250725130840.csv"
 columns = ["연령별(1)", "연령별(2)", "자살 사망자수 (명)", "자살률"]
 df = pd.read_csv(csv_path, encoding='utf-8', skiprows=3, names=columns)
@@ -18,12 +17,8 @@ df = df[~df["연령별(2)"].isin(["미상", "소계"])]
 st.subheader("📄 데이터 미리보기")
 st.dataframe(df, use_container_width=True)
 
-tab1, tab2 = st.tabs(["자살 사망자수", "자살률"])
-with tab1:
-    fig1 = px.bar(df, x="연령별(2)", y="자살 사망자수 (명)", title="연령별 자살 사망자수 (명)")
-    st.plotly_chart(fig1, use_container_width=True)
-with tab2:
-    fig2 = px.bar(df, x="연령별(2)", y="자살률", title="연령별 자살률")
-    st.plotly_chart(fig2, use_container_width=True)
+st.subheader("연령별 자살 사망자수 (명)")
+fig = px.bar(df, x="연령별(2)", y="자살 사망자수 (명)", title="연령별 자살 사망자수 (명)")
+st.plotly_chart(fig, use_container_width=True)
 
     
